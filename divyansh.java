9ebd6099e7536c57348fdf4d227b3891ad121f7e
Divyansh Agarwal <54850155+Divyansh1011@users.noreply.github.com>
2023-08-28 12:15:24 +0530

    Created divyansh.java
    Added Armstrong number program

---
+++ divyansh.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        scanner.close();
+
+        if (isArmstrong(number)) {
+            System.out.println(number + " is an Armstrong number.");
+        } else {
+            System.out.println(number + " is not an Armstrong number.");
+        }
+    }
+
+    // Function to check if a number is an Armstrong number
+    static boolean isArmstrong(int num) {
+        int originalNum = num;
+        int numberOfDigits = String.valueOf(num).length();
+        int sum = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            sum += Math.pow(digit, numberOfDigits);
+            num /= 10;
+        }
+
+        return sum == originalNum;
+    }
+}
